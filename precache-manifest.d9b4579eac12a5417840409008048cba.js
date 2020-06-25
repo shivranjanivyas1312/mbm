42081a56f848f5d33b6aad23f2d81b9c83bf3d17
shivranjanivyas1312 <vyasshivranjani@gmail.com>
2020-06-25 16:00:41 +0530

    Updates

---
+++ precache-manifest.d9b4579eac12a5417840409008048cba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45295570c588bcd2cbc92adf1da2443",
+    "url": "/mbm/index.html"
+  },
+  {
+    "revision": "6e3ef9b06b209d04dc6e",
+    "url": "/mbm/static/css/main.1c029319.chunk.css"
+  },
+  {
+    "revision": "ad9029332b825d4805ae",
+    "url": "/mbm/static/js/2.dc83811a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mbm/static/js/2.dc83811a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e3ef9b06b209d04dc6e",
+    "url": "/mbm/static/js/main.1302e770.chunk.js"
+  },
+  {
+    "revision": "324998b5b191c354c446",
+    "url": "/mbm/static/js/runtime-main.a8bdd015.js"
+  },
+  {
+    "revision": "5cd58a869c343cbe3505537daaccedae",
+    "url": "/mbm/static/media/background.5cd58a86.jpeg"
+  },
+  {
+    "revision": "4ade2d9a1b28503ea4d9c5b2172d1355",
+    "url": "/mbm/static/media/mobile.4ade2d9a.png"
+  },
+  {
+    "revision": "85b1575432cb0e2f0d5bf4a95230f4b9",
+    "url": "/mbm/static/media/pic.85b15754.jpeg"
+  },
+  {
+    "revision": "5ced44e89175ab1d989dd9f3cb015a59",
+    "url": "/mbm/static/media/resume.5ced44e8.pdf"
+  },
+  {
+    "revision": "c4489a5b2ae56d1a41428d10e1eb33e1",
+    "url": "/mbm/static/media/twitter.c4489a5b.png"
+  }
+]);
